dd04f8d2f31ce7ff9fb534cb9bd20811044ec1c7
msrecec <67606889+msrecec@users.noreply.github.com>
2021-10-05 16:32:30 +0200

    added interval observable

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { interval } from "rxjs";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"],
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    interval(1000).subscribe((count) => {
+      console.log(count);
+    });
   }
-
 }
